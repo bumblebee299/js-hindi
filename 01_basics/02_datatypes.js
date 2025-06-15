0dec26cf2236b6c91375264078c61a267b625449
Shrvan kumar <shrawan132005@gmail.com>
2025-06-15 13:37:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3
+    +
+    3) //  not a good practice, code readability should high
+
+console.log("shrawan")
+
+
+
+let name = "shrawan"
+let age = 20
+let isLoggedIn = false
+
+/* datatypes:-
+
+number => 2 to the power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined 
+symbol => unique
+
+object => very important
+
+*/
+
+console.log(typeof "phool");
+console.log(typeof age);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
