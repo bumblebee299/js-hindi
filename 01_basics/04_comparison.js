980925a7b03d06d9da34abce3d7d7f4bb1e7101a
Shrvan kumar <shrawan132005@gmail.com>
2025-06-16 07:32:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===   =>strict check, value as well as datatype
+
+console.log("2" === 2);
+
+console.log(true == 1);
+
