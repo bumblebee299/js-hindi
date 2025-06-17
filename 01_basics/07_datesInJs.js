4aad2a44051789156ec1509b0b83ba19efbb1135
Shrvan kumar <shrawan132005@gmail.com>
2025-06-17 17:17:21 +0000

    dates and times

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,27 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate)
+
+let myCreatedDate = new Date(2025,5,17,10,21)
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+myCreatedDate = new Date("06-17-2025")
+// console.log(myCreatedDate.toLocaleDateString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(myTimeStamp / 1000));
+
+let newDate = new Date("06-15-2025")
+// console.log(newDate.getMonth());
+// console.log(newDate.getTime());
+// console.log(newDate.getDay());
+
+
+
