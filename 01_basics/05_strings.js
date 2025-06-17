742919e5fff6f930409ccfd69f89ab30dfb22c4a
Shrvan kumar <shrawan132005@gmail.com>
2025-06-17 06:03:28 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "shrawan"
+const repoCount = 50
+// console.log(name + repoCount + "value");    // outdated
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("hiteshhc")
+console.log(gameName[1])
+//objects of string prototype
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t"));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(-8,4));
+console.log(gameName.includes("hitesh"))
+console.log(gameName.replace("hc","ch"));
+
+
+// const newString ="     hitesh     "
+// console.log(newString.trim());
+// console.log(newString.trimEnd());
+// console.log(newString.trimStart());
+
+// const game = new String("hitesh-hc-com")
+// console.log(game.split("-"));
+
+
+
+
+
+
+
