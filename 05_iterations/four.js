8fd60ed9f061477a0221be7474ff6bd0579fcf0e
Shrvan kumar <shrawan132005@gmail.com>
2025-06-24 17:50:01 +0000

    forin and forEach

---
+++ 05_iterations/four.js
@@ -0,0 +1,20 @@
+// forin 
+
+const myObject = {
+    js : "javascript",
+    cpp : "c++",
+    rb : "ruby",
+    swift : "swift by apple"
+}
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]); 
+   
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log(key);
+    console.log(programming[key]);
+    
+}
