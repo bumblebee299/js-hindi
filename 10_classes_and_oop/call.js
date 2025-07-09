0e82a22b09a5949beb7f67758e8bc09c19fe21fb
Shrvan kumar <shrawan132005@gmail.com>
2025-07-09 14:53:22 +0000

    call

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,12 @@
+function setUsername (username){
+    this.username = username
+    console.log('called');
+    
+}
+function createUser(username, email, password){
+    setUsername.call(this,username)
+    this.email = email
+    this.password = password
+}
+const chai = new createUser('shrawan', 'baba@gmail.com', '123')
+console.log(chai)
